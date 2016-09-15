34eef89a0a6fd791f2526e4d1da4a7f6f7777e71
Jay <jay.lingelbach@gmail.com>
2016-09-14 20:48:22 -0500

    updated examples layout

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?Location=Chicago'>Chicago, IL</Link>
+        </li>
+        <li>
+          <Link to='/?Location=Austin'>Austin, TX</Link>
+        </li>
+        <li>
+          <Link to='/?Location=London'>London, England</Link>
+        </li>
+      </ol>
     </div>
 
   )
